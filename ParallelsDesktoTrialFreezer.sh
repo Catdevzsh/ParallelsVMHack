d8dceb37b5e1648742b2efeedfb3624b2bed79f7
Flames Elite <84363801+FlamesLLC@users.noreply.github.com>
2021-11-01 10:54:42 -0700

    Add files via upload

---
+++ ParallelsDesktoTrialFreezer.sh
@@ -0,0 +1,7 @@
+import os
+import time
+import system
+echo "Freezing system in 3 seconds"
+## freeze the time of the app
+appclient.freezeTime()
+echo("Your Parallels Desktop 17 trial version has been frozen")
